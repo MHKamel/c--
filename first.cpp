979332e53ec8cbb93f1a18f005df9148e360a16b
MohamedHassan <mohamed.hassankamel.94@gmail.com>
2024-10-15 13:55:06 +0200

    first commit

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string user_name;
+    cout <<"Write your name:";
+    cin >> user_name;
+    cout << "welcome to C++" << user_name;
+    return 0;
+}
